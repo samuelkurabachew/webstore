e6441319a48436dfe253e274859c30ecdbf08779
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-03-14 13:39:36 -0500

    initail commit

---
+++ Order/src/main/java/webshop/order/controller/OrderController.java
@@ -0,0 +1,16 @@
+package webshop.order.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("order")
+public class OrderController {
+
+    @GetMapping
+    public ResponseEntity<?> test(){
+        return ResponseEntity.ok("test");
+    }
+}
