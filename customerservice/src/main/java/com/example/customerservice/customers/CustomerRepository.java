18fd02c4cd1b364944ef4dc650886f3dd0fcd6de
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-03-15 14:24:14 -0500

    remove the customer service branch

--- customerservice/src/main/java/com/example/customerservice/customers/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.customerservice.customers;
-
-import com.example.customerservice.customers.domain.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-}
