8f1a69663726fd30b3f7021e050015d05455556b
philipt <philiptilahun@gmail.com>
2022-03-15 13:12:08 -0500

    changed record types to classes

--- customerservice/src/main/java/com/example/customerservice/customers/domain/Address.java
+++ customerservice/src/main/java/com/example/customerservice/customers/domain/Address.java
@@ -1,6 +1,43 @@
 package com.example.customerservice.customers.domain;
 
-public record Address(String street,
-                      String city,
-                      String zip) {
+import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
+
+public class Address{
+        String street;
+        String city;
+        String zip;
+
+    public Address(){
+
+    }
+
+    public Address(String street, String city, String zip) {
+        this.street = street;
+        this.city = city;
+        this.zip = zip;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
 }
