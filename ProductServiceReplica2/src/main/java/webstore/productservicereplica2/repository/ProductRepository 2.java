f076f6363f886067dbc0ac14bdb1cec7e2805874
EYadeMA <yade.emiru@gmail.com>
2022-03-17 00:41:28 -0500

    just build

--- ProductServiceReplica2/src/main/java/webstore/productservicereplica2/repository/ProductRepository 2.java
+++
@@ -1,8 +0,0 @@
-package webstore.productservicereplica2.repository;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import webstore.productservicereplica2.domain.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
