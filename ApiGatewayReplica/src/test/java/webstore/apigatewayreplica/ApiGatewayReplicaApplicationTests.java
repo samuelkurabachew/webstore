1f8f0cd8e5d252c99077640d91ecf03405de4a55
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-03-16 22:02:01 -0500

    add api gateway replica

---
+++ ApiGatewayReplica/src/test/java/webstore/apigatewayreplica/ApiGatewayReplicaApplicationTests.java
@@ -0,0 +1,13 @@
+package webstore.apigatewayreplica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayReplicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
